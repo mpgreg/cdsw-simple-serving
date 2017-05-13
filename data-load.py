922d47193286efd40d48458c99ac19a01af1c562
Michael Gregory <mgregory@Michaels-MacBook-Pro.local>
2017-05-13 14:20:00 +0200

    added data-load.py and data-remove.py

---
+++ data-load.py
@@ -0,0 +1,14 @@
+#download the data
+!curl -o occupancy_data.zip http://archive.ics.uci.edu/ml/machine-learning-databases/00357/occupancy_data.zip
+
+#unzip the data
+!unzip occupancy_data.zip
+
+#make a directory in HDFS
+!hdfs dfs -mkdir /tmp/clouderanM
+
+#put the training data in HDFS
+!hdfs dfs -put -f datatraining.txt /tmp/clouderanM
+
+#delete the download remnants
+!rm occupancy_data.zip datatest.txt datatest2.txt datatraining.txt
