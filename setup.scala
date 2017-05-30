98fcf41fff22f89a32a8791725764d4f9c1231ab
Michael Gregory <mgregory@hadoop.cluster>
2017-05-30 16:25:07 +0000

    124

--- setup.scala
+++ setup.scala
@@ -1,2 +1,4 @@
 import sys.process._
 "wget http://apache.cs.uu.nl/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip" !
+"unzip apache-maven-3.5.0-bin.zip" !
+"rm apache-maven-3.5.0-bin.zip" !
